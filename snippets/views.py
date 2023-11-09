c90b249e2a7f1591165e650a6a6178707f32fdf3
suspiciousRaccoon <spablocabral@gmail.com>
2023-11-09 20:30:12 -0300

    Revert "refactor to use generic views"
    This reverts commit 41a48534db2f68c9af6e157588989ac02cf6158b.

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,40 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework import mixins
 from rest_framework import generics
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
