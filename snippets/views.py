9ef04b1ce2fb5551fb4155017131ff6b57c857ac
suspiciousRaccoon <spablocabral@gmail.com>
2023-11-09 21:49:00 -0300

    Add api root

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,17 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
